c87766e1048259a555081f8ab755fe63fc4df2d1
Paul Gibbs <djpaul@gmail.com>
2016-08-07 22:36:53 +0100

    asdasd

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,13 +13,13 @@ module.exports = function( grunt ) {
 					grunt.log.write( 'echo path: ' + stdout );
 				}
 			},
-			phpunit: {
+			/*phpunit: {
 				command: 'phpunit -v',
 				stdout: false,
 				callback: function (error, stdout) {
 					grunt.log.write( 'which phpunit: ' + stdout );
 				}
-			},
+			},*/
 
 			codecoverage: {
 				command: 'phpdbg -qrr phpunit -c /tmp/wordpress/codecoverage.xml --coverage-clover /tmp/wordpress/clover.xml'

7b6caf6e61f87157a06f12d08797ae3a8ad8db04
Paul Gibbs <djpaul@gmail.com>
2016-08-07 22:31:54 +0100

    asdasd

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,22 +14,15 @@ module.exports = function( grunt ) {
 				}
 			},
 			phpunit: {
-				command: 'which phpunit',
+				command: 'phpunit -v',
 				stdout: false,
 				callback: function (error, stdout) {
 					grunt.log.write( 'which phpunit: ' + stdout );
 				}
 			},
 
-
 			codecoverage: {
-				command: 'cd ~ && phpdbg -qrr phpunit -c /tmp/wordpress/codecoverage.xml --coverage-clover clover.xml',
-				options: {
-					shell: '/bin/bash'
-				},
-				callback: function (error, stdout) {
-					grunt.log.write( 'whtf cc: ' + stdout );
-				}
+				command: 'cd ~ && phpdbg -qrr phpunit -c /tmp/wordpress/codecoverage.xml --coverage-clover clover.xml'
 			}
 		}
 	});
